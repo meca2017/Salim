cc6ee620fdd1ec657b074b89f9b84930a5c7528c
meca2017 <mecatronicaturma2015@gmail.com>
2017-09-18 13:52:11 -0400

    main em php com consulta ao mysql 18/09 13:52

---
+++ main.php
@@ -0,0 +1,88 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+	<title>Salim - Sistema Automatizado Da Limpeza Dos Igarapés De Manaus</title>
+	<meta charset="utf-8">
+	<link rel="stylesheet" type="text/css" href="layout.css">
+</head>
+
+<body>
+	
+	<header style="width:100%;">
+		<h1><img src="img/logo.png" alt="Salim" title="Salim"></h1>
+	</header>
+	
+	<nav>
+		
+		<ul>
+			<li><a href="">home</a></li>
+			<li><a href="maps.php">maps</a></li>
+			<li><a href="boats.php">boats</a></li>
+			<li><a href="contacts.php">contacts</a></li>
+			<li><a href="about.php">about</a></li>			
+		</ul>		
+	
+	</nav>
+	
+	<section>
+
+		<div id="news" style="width:20%;height:100%;float:left;overflow-y:scroll;">
+
+			<header><h2>NEWS</h2></header>
+
+			<?php
+
+				$link = mysql_connect('localhost', 'root','');
+
+				if (!mysql_select_db('db_nickson', $link)) {
+				    echo 'Não foi possível selecionar o banco de dados';
+				    exit;
+				}
+
+				$sql    = 'SELECT nome FROM tb_login';
+				$result = mysql_query($sql, $link);
+
+				if (!$result) {
+				    echo "Erro do banco de dados, não foi possível consultar o banco de dados\n";
+				    echo 'Erro MySQL: ' . mysql_error();
+				    exit;
+				}
+
+				while ($row = mysql_fetch_assoc($result)) {				    
+				    echo"<a href='' style='position:relative;'>";
+					echo"	<article>";
+					echo"		<img src='img/logo.png'>";
+					echo"		<p>";
+					echo 			$row['nome'];
+					echo"		</p>";
+					echo"	</article>";					
+					echo"</a>";
+				}					
+			?>			
+
+		</div>
+		
+		<div id="googleMap" style="width:80%;height:100%;float:right;"></div>
+		
+		<script>
+			function myMap() 
+			{
+				var mapProp= {center:new google.maps.LatLng(-3.0404977,-59.9465168,13), zoom:5,};
+				var map=new google.maps.Map(document.getElementById("googleMap"),mapProp);
+			}
+		</script>
+
+		<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBkWUwfgXgmBCnOEnhp0sJHkl3vgoLP68U&callback=myMap"></script>		
+	
+	</section>
+	
+	<footer>
+
+		<img src="img/logo_fmm.png">
+	
+	</footer>
+
+</body>
+
+</html>

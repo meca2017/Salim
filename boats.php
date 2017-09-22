7321ab37768fe1fb572f65225dc249db097da842
meca2017 <mecatronicaturma2015@gmail.com>
2017-09-22 01:46:40 -0300

    boats 22/09 00:46

---
+++ boats.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>boats</title>
+	<meta charset="utf-8">
+	<link rel="stylesheet" type="text/css" href="layout.css">
+</head>
+<body>
+	<header>
+		<img src="img/logo.png">
+	</header>
+	<nav>
+		<ul>
+			<li><a href="main.php">home</a></li>
+			<li><a href="maps.php">maps</a></li>
+			<li><a href="boats.php">boats</a></li>
+			<li><a href="contacts.php">contacts</a></li>
+			<li><a href="about.php">about</a></li>			
+		</ul>	
+	</nav>
+	<section>
+		
+	</section>
+	<footer>
+		<img src="img/logo_fmm.png">
+	</footer>
+</body>
+</html>

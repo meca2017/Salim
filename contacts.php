6ba8a4e6d41e46d826f5da7d6377ba53a447212f
meca2017 <mecatronicaturma2015@gmail.com>
2017-09-22 01:47:25 -0300

    contacts 22/09 00:47

---
+++ contacts.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>contacts</title>
+	<meta charset="utf-8">
+	<link rel="stylesheet" type="text/css" href="layout.css">
+</head>
+<body>
+	<header>
+		<img src="img/logo.png">
+	</header>
+	<nav>
+		<ul>
+			<li><a href="main.html">home</a></li>
+			<li><a href="maps.html">maps</a></li>
+			<li><a href="boats.html">boats</a></li>
+			<li><a href="contacts.html">contacts</a></li>
+			<li><a href="about.html">about</a></li>			
+		</ul>	
+	</nav>
+	<section>
+		
+	</section>
+	<footer>
+		<img src="img/logo_fmm.png">
+	</footer>
+</body>
+</html>

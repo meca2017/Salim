80fe324d9e80e1a1675ade8c1b338cf609775913
meca2017 <mecatronicaturma2015@gmail.com>
2017-10-06 02:17:23 -0300

    index.php 06/10 01:17

--- index.php
+++ index.php
@@ -3,31 +3,37 @@
 <head>
 	<title>Salim - Login</title>
 	<meta charset="utf-8">
-	<link rel="stylesheet" type="text/css" href="layout_login.css">
+	<link rel="stylesheet" type="text/css" href="css/layout_index.css">
 </head>
 <body>
-	<header style="width:100%;">
-		<h1>
-			<img src="img/logo.png" alt="Salim" title="Salim">
-		</h1>
-	</header>
 
 	<section>
-		<div>
+		<div id="logo">
+			<img src="img/logo.png">
+		</div>
+
+		<div id="login">
 			<form autocomplete="on" method="post" action="login.php">
-				<strong>LOGIN</strong>
-				<input type="text" name="nome" placeholder="NOME" required="true">
-				</br>
-				<input type="password" name="senha" placeholder="PASSWORD" required="true">
-				</br>
-				<input type="submit" name="login" value="ENTRAR">
-				<a href="cadastrar.php">cadastrar</a>
+				<header class="titulo">
+					
+				</header>
+				<p>
+					<span class="fontawesome-user"></span>
+					<input type="text" name="nome" placeholder="NOME" required="true">
+				</p>
+				<p>
+					<span class="fontawesome-lock"></span>
+					<input type="password" name="senha" placeholder="SENHA" required="true">
+				</p>
+				<p><input type="submit" name="login" value="ENTRAR"></p>
+				<a href="cadastrar.php">Cadastrar</a>
 			</form>
 		</div>
 	</section>
 
 	<footer>
-		<img src="img/logo_fmm.png">	
+		<img src="">
 	</footer>
+
 </body>
 </html>

9ecfc1a5d6984b4aaef2278f9bf4d3832706e32f
meca2017 <mecatronicaturma2015@gmail.com>
2017-10-09 15:13:22 -0400

    main com mapa 09/10 15:13

--- main.php
+++ main.php
@@ -41,6 +41,22 @@
 			<a href="about.php"><li><p>about</p></li></a>			
 		</ul>	
 	</nav>
+
+	<div id="map"></div>
+
+	<script>
+	function myMap() {
+	var mapOptions = {
+	    center: new google.maps.LatLng(51.5, -0.12),
+	    zoom: 10,
+	    mapTypeId: google.maps.MapTypeId.HYBRID
+	}
+	var map = new google.maps.Map(document.getElementById("map"), mapOptions);
+	}
+	</script>
+	
+	<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyB2nbcDRU_E8zrYgZ8wWUg7dsVzfVmBpr0&callback=myMap"></script>
+
 </body>
 
 </html>
